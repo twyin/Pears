381f14edc49d884d6d5be016210533f63f415e54
twyin <tingyin1995@gmail.com>
2017-11-11 04:56:29 -0500

    Obstacle-handling in fill-1
    Goal is to add a list of indecies where there will be obstacles and then filling the board with patterns without overwritting the obstacles, or -1.
    Due to the logic behind fill-1 and fill-2, it was easier to add this feature to fill-1 so that was done. Need more time to plan out how to add this with fill-2.

--- src/pear/PearTester.java
+++ src/pear/PearTester.java
@@ -22,13 +22,39 @@ package pear;
 // fill2: given int[total], shuffle every icon, then put into board
 
 /* Conclusion:
- * 		Nov 10, 2017
- * 					On average fill-1 takes 0-10 millis, while fill-2 takes 0.
- * 					Shuffling 10 times takes about 0 millis
+ * 		Nov 10, 2017 3am
+ * 					on average fill-1 takes 0-10 millis, while fill-2 takes 0
+ * 					  shuffling 10 times takes about 0 millis
  * 		Still need to do
  * 					assert input is valid, and in other forms besides 
  * 					  just 2 int's separated by space..
  * 					make path finder that limit turns to maximum of 3
+ * 
+ * 		Nov 10, 2017 7pm
+ * 					realized that since paths can only be created over 0's,
+ * 					  therefore need to create ring of 0's around board.
+ * 					Adding obstacle-handling is easier to do for fill-1
+ * 					  than fill-2 so fill-1 now has the new feature. But
+ * 					  since fill-2 is proven to be faster, that will need
+ * 					  to be done (will probably time them against each other
+ * 					  gain after finishing obstacle-handling for both)
+ * 					this also makes shuffling more complicated..
+ * 					game ends when all the pairs have been found. Can make
+ * 					  program check if all coord's are 0's and -1's after
+ * 					  every valid move, or have number of pairs as 
+ * 					  instance variable of board class
+ * 					assert somewhere length of obstacles[] is even and no repeats
+ * 					  (might be unnecessary?)
+ * 					
+ * 					NEEDS IMMEDIATE FIX:
+ * 					HOW TO CREATE OBSTACLES AND NOT LET FILL AND SHUFFLE
+ * 					  CHANGE THEM
+ * 					how to stop game from running; right now posA_H 
+ * 					  runs out of bound before while condition becomes
+ * 					  false
+ * 					
+ * 		
+ * 			
  * 
  */
 		
@@ -42,17 +68,35 @@ class PearTester {
 		
 		Scanner keyboard = new Scanner(System.in);
 		
+		
+		
+		
+		int[] ob1 = {4,9,14,19};
+		TesterBoard tbSmall = new TesterBoard(5, 4, 5, ob1);
+		
+		
+// checking if new fill-1 can add obstacles properly
+		tbSmall.fill1();
+		System.out.println(tbSmall);
+		tbSmall.shuffle();
+		System.out.println(tbSmall);
+		
+
+		
+		
+		
 		long time1, time2;
 		
 		time1 = System.currentTimeMillis();
 		
-		TesterBoard tb1 = new TesterBoard(16, 12, 20);  
+		TesterBoard tb1 = new TesterBoard(16,12,20);
+		
 		// 192 squares, 96 pairs, 20 patterns named 1-20		
 		tb1.fill1();
 		time2 = System.currentTimeMillis();
 		System.out.println("time from fill-1: " + (time2-time1));
 
-		//System.out.println(tb1);
+		System.out.println(tb1);
 		
 		
 		time1 = System.currentTimeMillis();
@@ -79,12 +123,9 @@ class PearTester {
 		
 //		System.out.println(tb1);
 		
-		
-		TesterBoard tbSmall = new TesterBoard(5, 4, 5);
-		tbSmall.fill2();
-		tbSmall.shuffle();
-		System.out.println(tbSmall);
+
 // ======================================================================= PLAY
+/*
 		System.out.println("Please enter integers separated by a space");
 		System.out.println("Enter negative integer to quit");
 		int intA, intB;
@@ -107,6 +148,7 @@ class PearTester {
 			}
 			System.out.print(tbSmall);
 		} while (posA_H>=0);
+*/
 		
 		
 
--- src/pear/TesterBoard.java
+++ src/pear/TesterBoard.java
@@ -7,6 +7,8 @@ class TesterBoard {
 	private int width;
 	private int height;
 	private int patterns;
+	private int unsolvedPairs;
+	private int[] obstacles; // initialized to null
 	private int[][] board;
 	
 	
@@ -15,6 +17,17 @@ class TesterBoard {
 		this.width = w;
 		this.height = h;
 		this.patterns = p;
+		this.unsolvedPairs = w*h/2;
+		// obstacles is null
+		board = new int[h][w];
+	}
+	
+	protected TesterBoard(int w, int h, int p, int[] ob) {
+		this.width = w;
+		this.height = h;
+		this.patterns = p;
+		this.unsolvedPairs = w*h/2 - ob.length;
+		this.obstacles = ob;
 		board = new int[h][w];
 	}
 	
@@ -30,6 +43,25 @@ class TesterBoard {
 	
 	protected int getPatterns() {
 		return this.patterns;
+	}
+	
+	protected int getUnsolvedPairs() {
+		return this.unsolvedPairs;
+	}
+	
+	
+/* Obstacles is the "linear" INDEX at which they which
+ * For example, if desired obstacle is like:
+ *   [ 0 0 -1 ]
+ *   [ 0 0 -1 ]
+ *   [ 0 0 -1 ]
+ *   Then the desired boardArray[] used later in fill method should be:
+ *   {0, 0, -1, 0, 0, -1, 0, 0, -1}
+ *   Thus obstacles[] is:
+ *   {2,4,6}
+ */
+	protected int[] getObstacle() {
+		return this.obstacles;
 	}
 	
 	protected int getValue(int h, int w) {
@@ -54,12 +86,53 @@ class TesterBoard {
  *   board[][], where board[i][j] = boardArray[i*height+width].
  *   After boardArray[] is filled, its contents are copied over
  *   to board[][].
+ *   
+ * new fill-1 that can handle obstacles (Nov 11, 2017)
+ * First it makes an int array called boardArray with size equal to
+ *   the total amount of spaces in the board INCLUDING the obstacles.
+ *   For example, let a 3x3 board have obstacles[] of {2,5,8}
+ *   Then boardArray[] will be {0,0,-1,0,0,-1,0,0,-1}
+ *   Then the -1's from obstacle[] will be put into their respective
+ *   spot in boardArray. Afterwards, program will generate a random 
+ *   pattern and put it into two random but available locations in 
+ *   boardArray[]. After boardArray[] is filled, its contents are 
+ *   copied over to board[][] wherever there is no obstacle
+ *   
+ *   
+ *   !! Should assert that both board[][] boardArray[] reach their ends
+ *   
+ * 
  */
 	protected void fill1() {
 		int w = width;
+//
+		System.out.println("width: " + width);
 		int h = height;
-		int total = w*h;
+		int total = w*h;       // let w=5 h=4, total=20
+		// total is the number of spaces on the board INCLUDING OBSTACLES
 		int[] boardArray = new int[total];
+//
+		System.out.println("total = " + total);
+		
+		int obsLen;
+		if (obstacles==null) {
+			obsLen=0;
+		} else {
+			obsLen = obstacles.length;
+		}
+
+		// add obstacles into boardArray
+		// should work even if obstacle[] isn't in ascending order
+		for (int i=0; i<obsLen; i++) {
+			boardArray[obstacles[i]]=-1;
+		}
+		
+//
+		System.out.print("empty boardArray: ");
+		for (int i=0; i<total; i++) {
+			System.out.print(boardArray[i] + ", ");
+		}
+		System.out.println("");
 		
 		int p = patterns;
 		Random rand = new Random();
@@ -68,32 +141,55 @@ class TesterBoard {
 		int pos1;
 		int pos2;
 		
-		int counter = total/2;
-		System.out.println("total = " + total);
+		int counter = (total-obsLen)/2;
+//
+		System.out.println("counter: " + counter);
+		
 		for (; counter>0; counter--) {
-			r = rand.nextInt(p)+1;     // return 1-20 inclusive
-			pos1 = rand.nextInt(total);
-			
+			r = rand.nextInt(p)+1;        // return 1-5 inclusive
+//			
+//			System.out.print("r: " + r);
+			pos1 = rand.nextInt(total);   // generate[0:19]
+//
+//			System.out.print(" i1@" + pos1);
 			if (boardArray[pos1]==0) {
 				boardArray[pos1]=r;
-			} else {                   // else boardArray[pos1] is filled
-				while(boardArray[++pos1%total]!=0) { 
-				// this will find the next empty and let it = pos1
+			} else {                      // else boardArray[pos1] is filled
+				while(boardArray[++pos1%total]!=0) {
+				// pos1%total is because ++pos1 might eventually
+				//   increment it to bigger than total
+				//   (imagine the last few are filled, would 
+				//   need pos1 to re-route back to beginning)
+				// this while loop only stops when it finds a 0, which
+				//   is the next empty and let it = r
 				}
 				boardArray[pos1%total] = r;
 			}
+//
+//			System.out.print(" f1@" + pos1);
 			
 			pos2 = rand.nextInt(total);
+//
+//			System.out.print(" i2@" + pos2);
 			if (boardArray[pos2]==0) {
 				boardArray[pos2]=r;
 			} else {
-				while(boardArray[++pos2%total]!=0) { 
-				// this will find the next empty and let it = pos1
+				while(boardArray[++pos2%total]!=0) {
+				// this will find the next empty (which is 0)
 				}
 				boardArray[pos2%total] = r;
 			}
+//			System.out.println(" f2@" + pos2);
 		}
 		
+		
+		for (int i=0; i<total; i++) {
+			System.out.print(boardArray[i] + ", ");
+		}
+		
+		
+//
+		System.out.println("\nh, w: " + h + ", " + w);
 		for (int i=0; i<h; i++) {
 			for (int j=0; j<w; j++) {
 				board[i][j]=boardArray[i*w+j];
@@ -122,7 +218,7 @@ class TesterBoard {
 		int temp;
 		
 		for (int i=0; i<total; i++) {
-			boardArray[i] = i/2%20+1;
+			boardArray[i] = i/2%patterns+1;
 		}
 		
 		Random rand = new Random();
