665677dfa4bb6b4d6f5af90586bce1c0dd58776d
twyin <32972713+twyin@users.noreply.github.com>
2018-02-23 20:47:31 -0500

    Changed game from table to div
    Table, tr, and td elements created a problem where they would shift when a row or column was completed/matched. Therefore everything was made into div instead. Margins were added for separation purposes and to make elements look more like game pieces.

--- web_version/JS/PLevel.js
+++ web_version/JS/PLevel.js
@@ -27,8 +27,8 @@ let level1 = new PLevel(2, 3, 3, null, null);  // 7 by 10
 let ob2 = [16,27,38, 46,47,48,49,50,51,52, 60,71,82];  // 13 obstacles within 99 squares
 let level2 = new PLevel(9, 11, 14, ob2, null);
 
-let ob3 = [1,3,5,7,9,11];
-let level3 =  new PLevel(11, 16, 14, ob3, 300);
+let ob3 = [59, 23,27, 4,12, 19,31, 52,66, 86,100, 121,133, 157,165, 176,180, 195];
+let level3 =  new PLevel(12, 17, 14, ob3, 300);
 
-let game = [level1, level2, level3];
+let game = [level1, level3, level2];
 
