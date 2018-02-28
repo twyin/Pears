3c5c1ad240f22eaa97ea7287b3b0be2e542f4e70
twyin <32972713+twyin@users.noreply.github.com>
2018-02-28 04:34:42 -0500

    Updated effects on removed pairs
    Fixed an error where pairs that have been removed already can still appear to be clicked on. Also, the match function will no longer be called when either of the grid elements player has clicked on has value of 0.
    Better font styles and sizes were added to texts and integers on the board to increase user legibility.

--- web_version/JS/PLevel.js
+++ web_version/JS/PLevel.js
@@ -28,7 +28,7 @@ let ob2 = [16,27,38, 46,47,48,49,50,51,52, 60,71,82];  // 13 obstacles within 99
 let level2 = new PLevel(9, 11, 14, ob2, null);
 
 let ob3 = [59, 23,27, 4,12, 19,31, 52,66, 86,100, 121,133, 157,165, 176,180, 195];
-let level3 =  new PLevel(12, 17, 14, ob3, 300);
+let level3 =  new PLevel(12, 17, 18, ob3, 300);
 
 let game = [level1, level2, level3];
 
